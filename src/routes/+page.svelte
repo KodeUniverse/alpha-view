3eb53c4739a15e673822c84c6fd94c7147ec0c99
KodeUniverse <dakshin@gmail.com>
2025-05-30 17:00:59 -0400

    Added Home page & finance-dash page, with buttons to go between them

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,7 @@
-<h1 align='center'>AlphaView</h1>
-
+<h1>ALPHAVIEW HOME</h1>
 <script>
-    import data from '../go-services/scraping//data/finviz.json';
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
 </script>
 
-<table border="1" cellpadding="5" cellspacing="0">
-    <thead>
-        <tr>
-            {#each Object.keys(data[0]) as key}
-                <th>{key}</th>
-            {/each}
-        </tr>
-    </thead>
-    <tbody>
-        {#each data as row}
-            <tr>
-                {#each Object.values(row) as value}
-                    <td>{value}</td>
-                {/each}
-            </tr>
-        {/each}
-    </tbody>
-</table>
+<button onclick={() => goto('/finance-dash')}>Go to Finance Dashboard</button>
