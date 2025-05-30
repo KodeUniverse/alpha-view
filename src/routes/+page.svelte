bd565004e85f353c59e6367e578a5e7b5d061268
KodeUniverse <dakshin@gmail.com>
2025-05-30 15:38:13 -0400

    Added Svelte app, with home-page showing results of scraped JSON from back-end

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<h1 align='center'>AlphaView</h1>
+
+<script>
+    import data from '../go-services/scraping//data/finviz.json';
+</script>
+
+<table border="1" cellpadding="5" cellspacing="0">
+    <thead>
+        <tr>
+            {#each Object.keys(data[0]) as key}
+                <th>{key}</th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each data as row}
+            <tr>
+                {#each Object.values(row) as value}
+                    <td>{value}</td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
