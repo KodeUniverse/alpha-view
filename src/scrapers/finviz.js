7d6a4bff74d64df9828dfc51d9aa67d2b0443cf0
KodeUniverse <dakshin@gmail.com>
2025-10-01 00:49:34 -0400

    Fixed breaking bugs with finviz scraper, now functioning.

--- src/scrapers/finviz.js
+++ src/scrapers/finviz.js
@@ -4,13 +4,9 @@ import FileSystem from "fs";
 import cron from "node-cron";
 import redis from "redis";
 
-async function scrapeNews(source) {
+async function scrapeNews() {
 
-    sites = {
-        "finviz": "https://finviz.com/news.ashx",
-    };
-
-    headers = {
+    const headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
@@ -19,41 +15,40 @@ async function scrapeNews(source) {
         'Upgrade-Insecure-Requests': '1',
     };
 
-    const url = sites[source];
+    const url = "https://finviz.com/news.ashx"
 
     const htmlContent = await axios.get(url, { headers });
     const cheerio_obj = cheerio.load(htmlContent.data);
 
     const articles = [];
 
-    switch (source) {
-        case "finviz":
-            cheerio_obj("table.styled-table-new > tbody > tr.news_table-row").each((i, elem) => {
-                const headline = cheerio_obj(elem).find("td.news_link-cell > a.nn-tab-link").text();
-                const link = cheerio_obj(elem).find("td.news_link-cell > a.nn-tab-link").attr("href");
-                const _date = cheerio_obj(elem).find("td.news_date-cell").text();
 
-                let article = {
-                    title: headline,
-                    URL: link,
-                    date: _date
-                };
+    cheerio_obj("table.styled-table-new > tbody > tr.news_table-row").each((i, elem) => {
+        const headline = cheerio_obj(elem).find("td.news_link-cell > a.nn-tab-link").text();
+        const link = cheerio_obj(elem).find("td.news_link-cell > a.nn-tab-link").attr("href");
+        const _date = cheerio_obj(elem).find("td.news_date-cell").text();
 
-                articles.push(article);
-            });
-    }
-    
+        let article = {
+            title: headline,
+            URL: link,
+            date: _date
+        };
+
+        articles.push(article);
+    });
 }
+    
 
 async function main() {
 
     cron.schedule('*/1 * * * *', async () => {
         try {
+            console.log('Starting Finviz scrape...');
             // start scrape
             const data = await scrapeNews();
-
+            console.log('Finviz scrape succeeded!');
         } catch (error) {
-            console.error(`Finviz scrape failed: ${error}`);
+            console.error(`Finviz scrape failed: ${error.stack}`);
         }
     });
 }
