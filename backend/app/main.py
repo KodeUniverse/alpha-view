161331823624064be5a1b3d9779875f0d06e7e45
KodeUniverse <kodeuniverse@proton.me>
2026-09-01 20:17:12 -0400

    Fixed port issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -17,16 +18,12 @@ async def lifespan(app: FastAPI):
     # shutdown/clean up code. runs after API shutdown
 
 app = FastAPI(lifespan=lifespan)
-origins = [
-    "http://localhost",
-    "http://localhost:8080"
-]
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=[f"http://localhost:{os.environ.get('HOST_PORT', '5379')}"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
